ff5567b59ede65399d14529d1cfa92b2cc1067be
SIVARAMA KRISHNAN <sivatechnology001@gmail.com>
2021-11-11 17:05:26 +0530

    backend process

--- client/src/components/form/SuperAdminLogin.jsx
+++ client/src/components/form/SuperAdminLogin.jsx
@@ -1,7 +1,14 @@
 import './SuperAdminLogin.scss';
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 export default function SuperAdminLogin() {
+  useEffect(()=>{
+    axios.get("http://localhost:5000/users").then((response)=>
+    {
+      console.log(response.data);
+    })
+  })
   return (
     <div>
             <div className="container">
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const http = require('http');
 
 app.use(express.json());
-
+app.use(cors());
 const db=require('./models');
 
 
 //Routers
 const userRouter = require('./routes/Users');
 app.use('/users', userRouter);
+
 
 
 db.sequelize.sync().then(() =>
