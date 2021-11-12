5a80f6eda00a28167d4df92e943990c6f90753bd
gudsol <gudsol135@gmail.com>
2021-11-12 13:30:41 +0530

    add form

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import AdminHeader from './components/header/AdminHeader'
-function App() {
-  return (
-   <div>
-      <AdminHeader/>
-=======
-import * as AiIcons from 'react-icons/all';
 
 function App() {
   return (
    <div>
-      <AiIcons.MdMenuOpen />
-      <AiIcons.FaUserGraduate />
->>>>>>> dd5252c0d0da65b33141e908128297ecc3a2529c
+      <AdminHeader />
    </div>
   );
 }
--- client/src/components/header/AdminHeader.jsx
+++ client/src/components/header/AdminHeader.jsx
@@ -4,13 +4,27 @@ import './AdminHeader.scss';
 
 export default function AdminHeader() {
     return (
-        <div>
-            <div className="box1">
-              <h1>ADD STAFF FORM</h1>
-              <AiIcons.MdMenuOpen/> 
-                <div className="box2"></div>
-            </div>
-            
+        
+        <div className="container">
+            <div className="inner_container">
+                    <h1>STAFF ADDING</h1>
+                    <div className="box1">
+         <div className="name">
+             <p1>USERNAME*</p1>
+             <div className="ph1">
+                <AiIcons.AiOutlineUser className="user"/> <input className="us" type="text" placeholder="" required />
+             </div>
+         </div>
+         <div className="email">
+             <p1>E-MAIL*</p1>
+             <div className="ph2">
+                <AiIcons.AiOutlineUser className="mail"/> <input className="em" type="text" placeholder="" required />
+             </div>
+         </div>
+         </div>
+
+         </div>
         </div>
+            
     )
 }
