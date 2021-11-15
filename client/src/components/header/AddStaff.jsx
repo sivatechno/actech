43cfbe36a36ae41c9933dff8fe9fc307b7875a38
gudsol <gudsol135@gmail.com>
2021-11-15 17:19:28 +0530

    addstaff

--- client/src/components/header/AddStaff.jsx
+++ client/src/components/header/AddStaff.jsx
@@ -7,70 +7,87 @@ export default function AdminHeader() {
         
          <div className="container-one">
              <div className="inner_container">
-                       <AiIcons.IoClose className="close_icon"/>
-                    <p14>STAFF ADDING</p14> <hr />
+                           {/* heading start */}
+               <div className="inner_container_one">
+                     <div className="head_div">
+                     <p className="heading">ADD STAFF </p> 
+                     </div>
+                     <div className="close_icon_div">
+                      <AiIcons.IoClose className="close_icon"/>
+                    </div> 
+                    </div>
+                    <hr />   
+                                {/* heading end */}
+
+                    {/* ---------------------------------------------------------------------------------------------- */}
+                                       
                                                                    {/* FIRST CONTAINER START */}
-                        <div className="first_container">
-                            <div className="name">
-                                        <p className="inner_heading">USERNAME *</p>
-                                            <div className="textbox_one">
-                                              <AiIcons.AiOutlineUser className="user_icon"/> <input className="user" type="text" placeholder="" required />
+                        <div className="inner_container_two">
+                            <div className="sub_container_one">
+                                        <p className="sub_heading">USERNAME *</p>
+                                            <div className="sub_textbox">
+                                              <AiIcons.AiOutlineUser className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                             </div>
-                                            <span>* This is required field</span>
+                                            <span className="error">* This is required field</span>
                               </div>
-                                <div className="email">
-                                           <p className="inner_heading">E-MAIL *</p>
-                                              <div className="textbox_one">
-                                                 <AiIcons.AiOutlineMail className="user_icon"/> <input className="user" type="text" placeholder="" required />
+                                <div className="sub_container_one">
+                                           <p className="sub_heading">E-MAIL *</p>
+                                              <div className="sub_textbox">
+                                                 <AiIcons.AiOutlineMail className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                                  </div>
+                                                 <span className="error">* This is required field</span>
                                  </div>
                          </div>
                                                                          {/* FIRST CONTAINER END */}
 
                                                                          {/* SECOND CONTAINER START */}
-                      <div className="second_container">
-                            <div className="phone_no">
-                                        <p className="inner_heading">PHONE NUMBER *</p>
-                                            <div className="textbox_three">
-                                              <AiIcons.BsPhone className="phone_icon"/> <input className="phone" type="text" placeholder="" required />
+                      <div className="inner_container_two">
+                            <div className="sub_container_one">
+                                        <p className="sub_heading">PHONE NUMBER *</p>
+                                            <div className="sub_textbox">
+                                              <AiIcons.BsPhone className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                             </div>
+                                            <span className="error">* This is required field</span>
                               </div>
-                                <div className="pasword">
-                                           <p  className="inner_heading">PASSWORD*</p>
-                                              <div className="textbox_four">
-                                                 <AiIcons.AiOutlineEye className="eye_icon"/> <input className="pass" type="password" placeholder="" required />
+                                <div className="sub_container_one">
+                                           <p  className="sub_heading">PASSWORD *</p>
+                                              <div className="sub_textbox">
+                                                 <input className="textfield" type="password" placeholder="" required /> <AiIcons.AiOutlineEye className="inner_icon"/>
                                                  </div>
+                                                 <span className="error">* This is required field</span>
                                  </div>
                         </div>
 
                                                                            {/* SECOND CONTAINER END */}
                         
                                                                           {/* THIRD CONTAINER START */}
-                      <div className="third_container">
-                            <div className="Fname">
-                                        <p  className="inner_heading">FIRST NAME*</p>
-                                            <div className="textbox_five">
-                                              <AiIcons.RiUser6Fill className="fname_icon"/> <input className="firstname" type="text" placeholder="" required />
+                      <div className="inner_container_two">
+                            <div className="sub_container_one">
+                                        <p  className="sub_heading">FIRST NAME *</p>
+                                            <div className="sub_textbox">
+                                              <AiIcons.RiUser6Fill className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                             </div>
+                                            <span className="error">* This is required field</span>
                               </div>
-                                <div className="Lname">
-                                           <p  className="inner_heading">LAST NAME*</p>
-                                              <div className="textbox_six">
-                                                 <AiIcons.RiUser6Line className="lname_icon"/> <input className="lastname" type="text" placeholder="" required />
+                                <div className="sub_container_one">
+                                           <p  className="sub_heading">LAST NAME *</p>
+                                              <div className="sub_textbox">
+                                                 <AiIcons.RiUser6Line className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                                  </div>
+                                                 <span className="error">* This is required field</span>
                                  </div>
                         </div>
 
                                                                            {/* THIRD CONTAINER END */}
 
                                                                            {/* FORTH CONTAINER START */}
-                      <div className="fourth_container">
-                            <div className="cuntry">
-                                        <p  className="inner_heading">COUNTRY*</p>
-                                            <div className="textbox_seven">
-                                              <AiIcons.BiWorld className="world_icon"/> 
+                      <div className="inner_container_two">
+                            <div className="sub_container_one">
+                                        <p  className="sub_heading">COUNTRY *</p>
+                                            <div className="sub_textbox">
+                                              <AiIcons.BiWorld className="inner_icon"/> 
                                                                           {/* DROP DOWN */}
-                                                   <select className="country_list" >
+                                                    <select className="drop_down" >
                                                             <option className="list" value="COUNTRY">---COUNTRY---</option>
                                                                          <option className="list"  value="india">INDIA</option>
                                                                           <option className="list" value="usa">USA</option>
@@ -79,69 +96,81 @@ export default function AdminHeader() {
                                                      </select>
   
                                             </div>
+                                            <span className="error">* This is required field</span>
                               </div>
-                                <div className="cty">
-                                           <p  className="inner_heading">CITY*</p>
-                                              <div className="textbox_eight">
-                                                 <AiIcons.FaCity className="city_icon"/> <input className="city" type="text" placeholder="" required />
+                                <div className="sub_container_one">
+                                           <p  className="sub_heading">CITY *</p>
+                                              <div className="sub_textbox">
+                                                 <AiIcons.FaCity className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                                  </div>
+                                                 <span className="error">* This is required field</span>
                                  </div>
                         </div>
 
                                                                            {/* FORTH CONTAINER END */}
 
                                                                              {/* FIFTHCONTAINER START */}
-                      <div className="fifth_container">
-                            <div className="postalcode">
-                                        <p  className="inner_heading">POSTAL/ZIP CODE*</p>
-                                            <div className="textbox_nine">
-                                              <AiIcons.FaMapPin className="pin_icon"/> <input className="zipcode" type="text" placeholder="" required />
+                      <div className="inner_container_two">
+                            <div className="sub_container_one">
+                                        <p  className="sub_heading">POSTAL/ZIP CODE *</p>
+                                            <div className="sub_textbox">
+                                              <AiIcons.FaMapPin className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                             </div>
-                              </div>
+                                            <span className="error">* This is required field</span>
+                              </div> 
                                       {/* BROWSE BUTTON */}
-                                <div className="profile">
-                                           <p  className="inner_heading">AVATAR</p>
-                                              <div className="textbox_ten">
-                                                 <AiIcons.CgProfile className="prof_picture_icon"/>  <button className="browse_button">Browse</button>
+                                 <div className="sub_container_one">
+                                           <p  className="sub_heading">AVATAR</p>
+                                              <div className="sub_textbox">
+                                                 <AiIcons.CgProfile className="inner_icon"/>  <button className="browse_button">Browse</button>
                                                  </div>
+                                                 <span className="error">* This is required field</span>
                                  </div>
                         </div>
 
                                                                            {/* FIFTH CONTAINER END */}
 
                                                                             {/* SIXTH CONTAINER START */}
-                      <div className="sixth_container">
-                            <div className="adres">
-                                        <p  className="inner_heading">ADDRESS*</p>
-                                            <div className="textbox_eleven">
-                                              <AiIcons.FaRegAddressCard className="address_icon"/> <input className="address" type="text" placeholder="" required />
+                      <div className="inner_container_two">
+                            <div className="sub_container_one">
+                                        <p  className="sub_heading">ADDRESS *</p>
+                                            <div className="sub_textbox">
+                                              <AiIcons.FaRegAddressCard className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                             </div>
+                                            <span className="error">* This is required field</span>
                               </div>
-                                <div className="stat">
-                                           <p  className="inner_heading">PROVINCE/STATE*</p>
-                                              <div className="textbox_twele">
-                                                 <AiIcons.RiPinDistanceLine className="dualpin_icon"/> <input className="state" type="text" placeholder="" required />
+                                <div className="sub_container_one">
+                                           <p  className="sub_heading">PROVINCE/STATE *</p>
+                                              <div className="sub_textbox">
+                                                 <AiIcons.RiPinDistanceLine className="inner_icon"/> <input className="textfield" type="text" placeholder="" required />
                                                  </div>
+                                                 <span className="error">* This is required field</span>
                                  </div>
                         </div>
 
                                                                            {/* SIXTH CONTAINER END */}
 
-                                                                           <h2>ACCESS LEVEL</h2>
+                                                                          <div className="second_heading_div">
+                                                                           <p className="second_heading">ACCESS LEVEL</p>
+                                                                           </div>
 
                                                                            {/* SEVENTH CONTAINER START */}
-                    <div className="seventh_container">
-                                 <div className="aces_level">
+                    <div className="inner_container_three">
+                                 <div className="sub_container_two"> 
                                      {/* RADIO BUTTON START */}
-                                 <input type="radio" name="access_level"/>
-                                  <label className="label1">ADMIN</label> 
-                                  <input type="radio" name="access_level"/>
-                                  <label className="label1">STAFF</label><br/>
-                                   <p13>Admin has full rights. Staff has access to whatever is assigned to them.</p13> <br/>
+                                     <div className="radio_div">
+                                 <input className="radio_one" type="radio" name="access_level"/>
+                                  <label className="label_one">ADMIN</label> 
+                                
+                                  <input className="radio_two" type="radio" name="access_level"/>
+                                  <label className="label_two">STAFF</label><br/>
+                                    </div>
+                                    
+                                   <p className="info_content">Admin has full rights. Staff has access to whatever is assigned to them.</p> <br />
                                          {/* CHECK BOX */}
-                                   <input type="checkbox" name="notify_user" />
-                                   <label className="label1">NOTIFY USER</label><br/>
-                                   <p13>Send welcome email to this user.</p13> <br />
+                                    <input className="checkbox_one" type="checkbox" name="notify_user" />
+                                   <label className="label_three">NOTIFY USER</label> <br />
+                                   <p className="info_content">Send welcome email to this user.</p> <br />
                                    <textarea className="textarea" name="notes" id="" cols="93" rows="10"></textarea>
                                  </div>
                                  
@@ -151,16 +180,16 @@ export default function AdminHeader() {
 
                                                                              {/* EIGHTH CONTAINER START */}
 
-                     <div className="eighth_container">
+                      <div className="inner_container_four">
                         <div className="final_buttons">
                              <button className="cancel_staff">CANCEL</button>
                               <button className="add_staff">ADD STAFF</button>
                         </div>
-                    </div>
+                    </div> 
                                                                             {/* EIGHTH CONTAINER START */}
 
-                </div>
-          </div>
+            </div>    </div>
+          
             
     )
 }
