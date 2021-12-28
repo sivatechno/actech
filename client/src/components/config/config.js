8be4cfb877ac123c8a48791c1dcbe2b701428f61
GUNASEKARAN <gudsol135@gmail.com>
2021-12-28 23:27:43 +0530

    error_clear

--- client/src/components/config/config.js
+++ client/src/components/config/config.js
@@ -1,10 +1,10 @@
 const dev = {
-    API_URL: "http://localhost:5000"
+    API_URL: "http://ec2-3-110-115-5.ap-south-1.compute.amazonaws.com:5000/"
     // API_URL: "http://ec2-3-110-115-5.ap-south-1.compute.amazonaws.com:5000/"
 }
 
 const prod = {
-    API_URL: "http://localhost:5000"
+    API_URL: "http://ec2-3-110-115-5.ap-south-1.compute.amazonaws.com:5000/"
     // API_URL: "http://ec2-3-110-115-5.ap-south-1.compute.amazonaws.com:5000/"
 }
 const config = process.env.NODE_ENV === 'development' ? dev : prod
